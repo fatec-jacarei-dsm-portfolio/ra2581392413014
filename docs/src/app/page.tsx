c15f7e6ef19086964a3127fefa667db41d8bbebc
lucasroqe <alvim.lucas2@hotmail.com>
2025-05-19 21:11:11 -0300

    init

---
+++ docs/src/app/page.tsx
@@ -0,0 +1,176 @@
+import BlurFade from '@/components/magicui/blur-fade'
+import BlurFadeText from '@/components/magicui/blur-fade-text'
+import { ProjectCard } from '@/components/project-card'
+import { ResumeCard } from '@/components/resume-card'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { DATA } from '@/data/resume'
+import Link from 'next/link'
+import Markdown from 'react-markdown'
+
+const BLUR_FADE_DELAY = 0.04
+
+export default function Page() {
+  return (
+    <main className="flex min-h-[100dvh] flex-col space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-3xl">
+          <div className="flex justify-between">
+            <div className="flex flex-1 flex-col space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-2xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none"
+                yOffset={8}
+                text={`Olá, me chamo ${DATA.name.split(' ')[0]} 👋`}
+              />
+              <BlurFadeText
+                className="my-4 max-w-[600px] font-light md:text-2xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-16 md:size-32">
+                <AvatarImage
+                  alt={DATA.name}
+                  src={DATA.avatarUrl}
+                  className="scale-125 object-contain"
+                />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="mb-4 text-center text-2xl font-bold">Sobre mim</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="text-center text-muted-foreground md:text-left md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      <section id="skills">
+        <div className="flex flex-col">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="mb-4 text-center text-2xl font-bold">Tecnologias</h2>
+          </BlurFade>
+          <div className="flex flex-wrap justify-center gap-2">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 6 + id * 0.05}>
+                <Badge className="text-sm">{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="projects">
+        <div className="w-full">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <div className="justify-centertext-center flex flex-col items-center">
+              <h2 className="mb-4 text-center text-2xl font-bold">
+                Confira meus últimos projetos
+              </h2>
+              <p className="mb-2 text-center text-muted-foreground md:text-left md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Atuei no desenvolvimento de projetos acadêmicos e pessoais.
+                Confira:
+              </p>
+            </div>
+          </BlurFade>
+          <div className="mx-auto grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="mb-4 text-center text-2xl font-bold">
+              Formação acadêmica
+            </h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 10 + id * 0.05}
+            >
+              <ResumeCard
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <h2 className="mb-4 text-center text-2xl font-bold">
+              Experiência profissional
+            </h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+            >
+              <ResumeCard
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? 'Present'}`}
+                description={work.description}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="contact">
+        <div className="mb-12 mt-14 grid w-full items-center justify-center text-center md:mb-0 md:px-6">
+          <BlurFade delay={BLUR_FADE_DELAY * 13}>
+            <div className="space-y-4">
+              <h2 className="text-2xl font-bold tracking-tighter sm:text-4xl">
+                Fale comigo
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Quer bater um papo? Me mande um{' '}
+                <Link
+                  href="mailto:alvim.lucas2@hotmail.com"
+                  className="text-blue-500 hover:underline"
+                >
+                  e-mail
+                </Link>{' '}
+                e irei te responder o mais rápido possível
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  )
+}
---
+++ docs/src/data/resume.tsx
@@ -0,0 +1,208 @@
+import { Icons } from '@/components/icons'
+import { HomeIcon, NotebookIcon } from 'lucide-react'
+
+export const DATA = {
+  name: 'Lucas Roque',
+  initials: 'LR',
+  url: ' ',
+  location: 'Jacareí, SP',
+  locationLink: 'https://www.google.com/maps/place/jacarei',
+  description: 'Estudante em Desenvolvimento de Software em Multiplataforma.',
+  summary:
+    'Apaixonado por tecnologia, estou focado em criar soluções eficientes e inovadoras, desde o design da interface até a implementação de funcionalidades escaláveis.',
+  avatarUrl: '/me.jpg',
+  skills: [
+    'React',
+    'Next.js',
+    'Javascript',
+    'Typescript',
+    'Node.js',
+    'TailwindCSS',
+    'Python',
+    'Java',
+    'Postgres',
+    'MongoDB',
+  ],
+  navbar: [{ href: '/', icon: HomeIcon, label: 'Home' }],
+  contact: {
+    email: 'alvim.lucas2@hotmail.com',
+    tel: '+5512992152060',
+    social: {
+      GitHub: {
+        name: 'GitHub',
+        url: 'https://github.com/lucasroqe',
+        icon: Icons.github,
+        navbar: true,
+      },
+      LinkedIn: {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/lucasroqe/',
+        icon: Icons.linkedin,
+        navbar: true,
+      },
+      Email: {
+        name: 'Email',
+        url: 'mailto:alvim.lucas2@hotmail.com',
+        icon: Icons.email,
+        navbar: true,
+      },
+    },
+  },
+  work: [
+    {
+      company: 'Redt',
+      href: 'https://redt.cloud/',
+      badges: [],
+      location: 'São José dos Campos, SP',
+      title: 'Analista de TI',
+      logoUrl: '/redt.png',
+      start: '05/2022',
+      end: '08/2024',
+      description:
+        'Suporte técnico remoto e presencial para equipamentos de videoconferência e telefonia na Embraer S.A, incluindo atendimento a vice-presidentes, diretores e executivos de alto escalão.',
+    },
+    {
+      company: 'Net Jacareí',
+      href: 'https://netjacarei.com.br/',
+      badges: [],
+      location: 'Jacareí, SP',
+      title: 'Analista de suporte técnico N2',
+      logoUrl: '/netjacarei.png',
+      start: '01/2021',
+      end: '05/2022',
+      description:
+        'Suporte técnico N2 via telefone, e-mail e WhatsApp, oferecendo atendimento de qualidade para clientes com links de conexão empresariais e residenciais, solucionando problemas técnicos relacionados à conectividade e ao funcionamento de serviços de internet.',
+    },
+  ],
+  education: [
+    {
+      school: 'Fatec - Jacareí',
+      href: 'https://fatec.sp.gov.br/',
+      degree: 'Técnologo em Desenvolvimento de Software em Multiplataforma',
+      logoUrl: '/fatec.jpg',
+      start: '01/2024',
+      end: '12/2026',
+    },
+    {
+      school: 'UNICSUL',
+      href: 'https://www.cruzeirodosul.edu.br/',
+      degree: 'Técnologo em Análise e Desenvolvimento de Sistemas',
+      logoUrl: '/unicsul.png',
+      start: '01/2020',
+      end: '01/2023',
+    },
+    {
+      school: 'Etec Cônego José Bento',
+      href: 'https://www.cps.sp.gov.br/etec/',
+      degree: 'Técnico em Redes de Computadores',
+      logoUrl: '/etec.jpg',
+      start: '01/2020',
+      end: '06/2021',
+    },
+    {
+      school: 'Etec Cônego José Bento',
+      href: 'https://www.cps.sp.gov.br/etec/',
+      degree: 'Técnico em Redes de Computadores integrado ao ensino médio',
+      logoUrl: '/etec.jpg',
+      start: '01/2016',
+      end: '12/2018',
+    },
+  ],
+  projects: [
+    {
+      title: 'Spendo',
+      href: 'https://github.com/lucasroqe/spendo',
+      dates: '2025',
+      active: true,
+      description:
+        'Desenvolvido para ajudar a gerenciar gastos pessoais. Com uma interface simples e amigável, permite o acompanhamento de despesas, categorização e visualização de hábitos financeiros por meio de gráficos intuitivos.',
+      technologies: [
+        'Next.js',
+        'Typescript',
+        'SQLite',
+        'Prisma',
+        'TailwindCSS',
+        'Shadcn UI',
+        'BetterAuth',
+      ],
+      links: [
+        {
+          type: 'Repositório',
+          href: 'https://github.com/lucasroqe/spendo',
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: '/spendo.png',
+      video: '',
+    },
+    {
+      title: 'NutriMe',
+      href: 'https://github.com/crewTech-ABP/NutriMe',
+      dates: '2024',
+      active: true,
+      description:
+        'Gerenciamento de alimentação e nutrição, permitindo que os usuários registrem suas refeições, monitorem suas metas nutricionais e tenham acompanhamento de nutricionistas.',
+      technologies: [
+        'React',
+        'Typescript',
+        'Node.js',
+        'Axios',
+        'PostgreSQL',
+        'TailwindCSS',
+      ],
+      links: [
+        {
+          type: 'Repositório',
+          href: 'https://github.com/crewTech-ABP/NutriMe',
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: '/nutrime.png',
+      video: '',
+    },
+    {
+      title: 'Golden Hive Solutions',
+      href: 'https://github.com/lucasroqe/golden-hive',
+      dates: '2024',
+      active: true,
+      description:
+        'Sistema web didático para apresentar a metodologia ágil Scrum.',
+      technologies: ['Javascript', 'HTML5', 'CSS3', 'PostgreSQL'],
+      links: [
+        {
+          type: 'Repositório',
+          href: 'https://github.com/lucasroqe/golden-hive',
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: '/golden-hive.jpg',
+      video: '',
+    },
+    {
+      title: 'Auth Form',
+      href: 'https://github.com/lucasroqe/auth-form',
+      dates: '2024',
+      active: true,
+      description:
+        'Formulário de autenticação com login/registro pronto para utilizar.',
+      technologies: [
+        'Next.js',
+        'Typescript',
+        'SQLite',
+        'Prisma',
+        'Auth.js',
+        'Zod',
+        'Shadcn UI',
+      ],
+      links: [
+        {
+          type: 'Repositório',
+          href: 'https://github.com/lucasroqe/auth-form',
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: '/auth-form.jpg',
+      video: '',
+    },
+  ],
+} as const
